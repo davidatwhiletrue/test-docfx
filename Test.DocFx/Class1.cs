281c1d81f3a8d54b15cefd127a80c8e77dd5791d
David Hernando Davalillo <davidhernando@gmail.com>
2021-12-19 15:43:30 +0100

    initial commit

---
+++ Test.DocFx/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace Test.DocFx;
+
+/// <summary>
+/// Class 1 description
+/// </summary>
+public class Class1
+{
+    /// <summary>
+    /// A public property
+    /// </summary>
+    public string Prop1 { get; init; }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="prop1"></param>
+    public Class1(string prop1)
+    {
+        this.Prop1 = prop1;
+    }
+}
